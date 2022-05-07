47212b830df338cd8276c3728f55e621da405276
Claudiu <clau_123@yahoo.com>
2022-05-07 11:37:29 +0300

    add screens

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+import EmptyHistoryScreen from "./screens/EmptyHistoryScreen";
+import MainScreen from "./screens/MainScreen";
+import HistoryScreen from "./screens/HistoryScreen";
+
+const Stack = createStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Screen name="Notifications" component={HistoryScreen} />
+        <Stack.Screen name="Profile" component={EmptyHistoryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
---
+++ screens/EmptyHistoryScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const EmptyHistoryScreen = () => {
+  return (
+    <View>
+      <Text>Empty History Screen</Text>
+    </View>
+  );
+};
+
+export default EmptyHistoryScreen;
